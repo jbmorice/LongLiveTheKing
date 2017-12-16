4f82accf298ffddc42d7afacc33560a9afa05046
Guillaume Versal <Ahziel@users.noreply.github.com>
2017-12-16 18:55:22 +0100

    Add route generation
    Add route automatic generation at init. Change the road prefab.
    Change the main scene with the new prefab

--- LongLiveTheKing/Assets/Scripts/UnityComponents/RoadComponent.cs
+++ LongLiveTheKing/Assets/Scripts/UnityComponents/RoadComponent.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,6 +12,39 @@ public class RoadComponent : MonoBehaviour
     public void Init()
     {
         Road = new Road(this.gameObject, FirstVillage.Village, SecondVillage.Village);
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+        Vector3 position = (FirstVillage.transform.position + SecondVillage.transform.position) / 2;
+        cube.transform.position = new Vector3(position[0],0.05f,position[2]);
+
+        cube.transform.localScale = new Vector3(Vector3.Distance(FirstVillage.transform.position, SecondVillage.transform.position), 0.1f,0.5f);
+
+        Vector3 vector = position - SecondVillage.transform.position;
+        double angle;
+        if (vector.normalized[2] < 0 && vector.normalized[0] < 0)
+        {
+            angle = (Math.Acos(vector.normalized[0]) * 180 / Math.PI);
+            Debug.Log("if 1 " + angle);
+        }
+        else if (vector.normalized[2] < 0 && vector.normalized[0] > 0)
+        {
+            angle = (Math.Acos(vector.normalized[0]) * 180 / Math.PI);
+            Debug.Log("if 2 " + angle);
+        }
+        else if (vector.normalized[2] > 0 && vector.normalized[0] < 0)
+        {
+            angle = -(Math.Acos(vector.normalized[0]) * 180 / Math.PI);
+            Debug.Log("if 3 " + angle);
+        }
+        else
+        {
+            angle = -(Math.Acos(vector.normalized[0]) * 180 / Math.PI);
+            Debug.Log("if 4" + angle);
+        }
+        Debug.Log(position);
+
+
+        cube.transform.eulerAngles = new Vector3(0.0f,(float)angle,0.0f);
     }
 
     void Start ()
