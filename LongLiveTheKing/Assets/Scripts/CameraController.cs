abcb25e61bee4a6eab23cd3767fade3d50934205
Jean-Baptiste Morice <mail@jbmorice.com>
2018-01-08 12:13:44 +0100

    Added CameraController.cs and implemented drag and drop input for army movement

---
+++ LongLiveTheKing/Assets/Scripts/CameraController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+
+public class CameraController : MonoBehaviour
+{
+    public bool ClickHold = false;
+    VillageComponent sourceVillage = null;
+    VillageComponent destinationVillage = null;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    void MoveArmy()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ClickHold = true;
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                sourceVillage = hit.transform.gameObject.GetComponent<VillageComponent>();
+                if (sourceVillage == null) return;
+                Debug.Log("Source village : " + sourceVillage.name);
+            }
+        }
+        if (Input.GetMouseButtonUp(0) && sourceVillage != null)
+        {
+            ClickHold = false;
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                destinationVillage = hit.transform.gameObject.GetComponent<VillageComponent>();
+                if (destinationVillage == null) return;
+                Debug.Log("Destination village : " + destinationVillage.name);
+            }
+
+            // #TODO: Moving logic
+
+            // Reset attribute
+            sourceVillage = null;
+            destinationVillage = null;
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    MoveArmy();
+	}
+}
