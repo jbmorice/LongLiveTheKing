680fb18f75f97d3d02fadc9b3b2352edec9fd376
Jean-Baptiste Morice <mail@jbmorice.com>
2018-01-23 14:53:22 +0100

    Added PopulationDiminution.cs

---
+++ LongLiveTheKing/Assets/Scripts/PopulationDiminution.cs
@@ -0,0 +1,27 @@
+﻿public class PopulationDiminution : AgentBehaviour
+{
+    private Village _village;
+    private float _period = 1.0f;
+    private float _elapsedTime = 0.0f;
+    private int _decrement = 1;
+
+    public bool Start(Village village)
+    {
+        if (base.Start())
+        {
+            _village = village;
+            return true;
+        }
+        return false;
+    }
+
+    public override void Update(float dt)
+    {
+        _elapsedTime += dt;
+        if (_elapsedTime > _period)
+        {
+            _village.Population -= _decrement;
+            _elapsedTime -= _period;
+        }
+    }
+}
